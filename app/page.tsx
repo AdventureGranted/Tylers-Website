6389c48e7d709f7bb0b97602932131cf884175cf
Tyler Grant <Tylerbb812@yahoo.com>
2025-06-19 18:28:36 -0600

    added my picture and message that site is under development

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ProfileCard from "./components/ProfileCard";
 import TechnicalSkills from "./components/TechnicalSkills";
 import WorkExperienceCard from "./components/WorkExperienceCard";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-gray-900 relative">
       <main className="px-6 pt-4 md:px-24 text-gray-200">
         <div className="">
           <ProfileCard />
@@ -13,6 +14,24 @@ export default function Home() {
           <WorkExperienceCard />
         </div>
       </main>
+      {/* Floating Profile + Chat Bubble */}
+      <div className="fixed bottom-6 right-6 flex items-end z-50">
+        <div className="relative">
+          <Image
+            src="/profile.jpg"
+            alt="Profile Picture of Tyler"
+            width={64}
+            height={64}
+            className="rounded-full border-4 border-black shadow-lg"
+          />
+          {/* Chat bubble */}
+          <div className="absolute -left-56 bottom-2 flex items-center">
+            <div className="bg-gray-900 text-gray-200 text-sm font-semibold px-4 py-2 rounded-2xl shadow-lg animate-bounce border-2 border-black relative after:content-[''] after:absolute after:top-1/2 after:right-[-16px] after:-translate-y-1/2 after:border-8 after:border-transparent after:border-l-yellow-300">
+              Site is under development
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
